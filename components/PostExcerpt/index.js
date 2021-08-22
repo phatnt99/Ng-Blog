6c8172dca4ea1e99298996a5682b3dcbe6f3f07e
Phat Nguyen <phatnguyen2499@gmail.com>
2021-08-22 10:57:31 +0700

    change style home index

--- components/PostExcerpt/index.js
+++ components/PostExcerpt/index.js
@@ -1,6 +1,6 @@
 export default function PostExcerpt({ excerpt }) {
     return (
-        <div className="w-full text-lg font-medium leading-normal sm:text-2xl">
+        <div className="w-full leading-normal">
             {excerpt}
         </div>
     );
--- components/PostPreview/index.js
+++ components/PostPreview/index.js
@@ -4,7 +4,7 @@ import PostPreviewTitle from "../PostPreviewTitle";
 
 export default function PostPreview({ post }) {
     return (
-        <div className="flex flex-col my-4 space-y-3 bg-white p-5 border-2 border-black">
+        <div className="flex flex-col my-4 space-y-3 bg-white p-5 border-2 rounded-lg">
             <PostPreviewTitle slug={post.slug} title={post.title} />
             <PostDetails author={post.author} date={post.date} />
             <PostExcerpt excerpt={post.excerpt} />
